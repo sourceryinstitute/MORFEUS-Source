9b011c66a184458f9c347e0c3779a0e4efc66bcf
Izaak Beekman <zbeekman@gmail.com>
2019-07-01 20:54:10 -0400

    GHActions: Remove debug info from mirror script

--- .github/main.workflow.sh
+++ .github/main.workflow.sh
@@ -2,17 +2,6 @@
 
 set -o errexit
 
-if jq --version && echo "Have jq" ; then
-  echo "Printing json event for debugging..."
-  jq -r . "$GITHUB_EVENT_PATH"
-else
-  echo "No jq, just dumping ugly JSON"
-  echo ""
-  cat "$GITHUB_EVENT_PATH"
-  echo ""
-  echo ""
-fi
-
 # configure git
 git config --global user.name "Sourcery-Bot"
 git config --global user.email "si-bot@izaakbeekman.com"
