adeccbaa6774177c7d6a187f28f0f5d170ea2ac6
Damian Rouson <damian@sourceryinstitute.org>
2019-10-18 17:09:45 -0700

    FD material label logic: add z direction

--- src/FD/tests/unit/material-map.F90
+++ src/FD/tests/unit/material-map.F90
@@ -14,11 +14,11 @@ program main
   !! via ctest.
   implicit none
 
-  integer i,j
+  integer i, j, k
 
   type material_t
     character(len=len('burrito')), dimension(:), allocatable :: material_name
-    integer, dimension(:), allocatable :: nx_blocks, ny_blocks
+    integer, dimension(:), allocatable :: nx_blocks, ny_blocks, nz_blocks
   end type
 
   type(material_t) core, wrapper
@@ -31,42 +31,53 @@ program main
   wrapper%material_name = [character(len=len('burrito')) :: 'bag', 'air', 'foil']
   core%nx_blocks = [1]
   core%ny_blocks = [2]
+  core%nz_blocks = [1]
   wrapper%nx_blocks = [3,2,1]
   wrapper%ny_blocks = [1,2,3]
+  wrapper%nz_blocks = [2]
+
+  if (wrapper%nz_blocks(1) < core%nz_blocks(1)) error stop "burrito not fully covered"
 
   associate( &
     nx => core%nx_blocks(1) + symmetry*sum(wrapper%nx_blocks), &
-    ny => core%ny_blocks(1) + symmetry*sum(wrapper%ny_blocks) )
+    ny => core%ny_blocks(1) + symmetry*sum(wrapper%ny_blocks), &
+    nz => wrapper%nz_blocks(1) )
 
     allocate( mat(nx,ny) )
 
-    do j=1,ny
-      do i=1,nx
-        mat(i,j) = material(i, j, core, wrapper)
+    do k=1,nz
+      do j=1,ny
+        do i=1,nx
+          mat(i,j) = material(i, j, k, core, wrapper)
+        end do
+        write(*,csv_format) mat(:,j)
       end do
-      write(*,csv_format) mat(:,j)
+      write(*,*)
     end do
 
   end associate
 
 contains
 
-  function material(ix, iy, core_, wrapper_) result(material_ix_iy)
-    integer, intent(in) :: ix, iy
+  function material(ix, iy, iz, core_, wrapper_) result(material_ix_iy)
+    integer, intent(in) :: ix, iy, iz
     type(material_t), intent(in) :: core_, wrapper_
     character(len=:), allocatable :: material_ix_iy
+    character(len=len(core_%material_name)), parameter :: void_name="void"
 
-    associate( &
-      nx_layers => [wrapper_%nx_blocks, core_%nx_blocks, wrapper_%nx_blocks(size(wrapper_%nx_blocks):1:-1) ], &
-      ny_layers => [wrapper_%ny_blocks, core_%ny_blocks, wrapper_%ny_blocks(size(wrapper_%ny_blocks):1:-1) ], &
-      material => [wrapper_%material_name, core_%material_name, wrapper_%material_name(size(wrapper_%material_name):1:-1)] )
-
+    associate( core_material_name => merge( core_%material_name, void_name, iz <= core_%nz_blocks ) )
       associate( &
-        block_material_x => [( [(material(i), j=1,nx_layers(i))], i=1,size(nx_layers) )], &
-        block_material_y => [( [(material(i), j=1,ny_layers(i))], i=1,size(ny_layers) )] )
+        nx_layers => [wrapper_%nx_blocks, core_%nx_blocks, wrapper_%nx_blocks(size(wrapper_%nx_blocks):1:-1) ], &
+        ny_layers => [wrapper_%ny_blocks, core_%ny_blocks, wrapper_%ny_blocks(size(wrapper_%ny_blocks):1:-1) ], &
+        material => [wrapper_%material_name, core_material_name, wrapper_%material_name(size(wrapper_%material_name):1:-1)] )
 
-        associate( wrapper_material_x => replace_layers(block_material_x, block_material_y, iy) )
-          material_ix_iy = wrapper_material_x(ix)
+        associate( &
+          block_material_x => [( [(material(i), j=1,nx_layers(i))], i=1,size(nx_layers) )], &
+          block_material_y => [( [(material(i), j=1,ny_layers(i))], i=1,size(ny_layers) )] )
+
+          associate( wrapper_material_x => replace_layers(block_material_x, block_material_y, iy) )
+            material_ix_iy = wrapper_material_x(ix)
+          end associate
         end associate
       end associate
     end associate
