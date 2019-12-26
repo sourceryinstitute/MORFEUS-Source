e33ec7e25cbf3db9eef99f31a9890ed606afa49d
Damian Rouson <damian@sourceryinstitute.org>
2019-12-26 11:17:04 -1000

    FD: add stub hallo exchange test

---
+++ src/FD/grid/inbox_interface.f90
@@ -0,0 +1,18 @@
+!
+!     (c) 2019 Guide Star Engineering, LLC
+!     This Software was developed for the US Nuclear Regulatory Commission (US NRC)
+!     under contract "Multi-Dimensional Physics Implementation into Fuel Analysis under
+!     Steady-state and Transients (FAST)", contract # NRC-HQ-60-17-C-0007
+!
+module inbox_interface
+  !! author: Damian Rouson
+  !! date:
+  !! Encapsulate information and procedures for structured_grid block halo exchanges
+  implicit none
+
+  private
+  public :: inbox
+
+  type inbox
+  end type
+end module
---
+++ src/FD/tests/unit/test-halo-exchange.f90
@@ -0,0 +1,12 @@
+!
+!     (c) 2019 Guide Star Engineering, LLC
+!     This Software was developed for the US Nuclear Regulatory Commission (US NRC)
+!     under contract "Multi-Dimensional Physics Implementation into Fuel Analysis under
+!     Steady-state and Transients (FAST)", contract # NRC-HQ-60-17-C-0007
+!
+program main
+  use inbox_interface, only : inbox
+  implicit none
+  type(inbox) :: halo
+  print *,"Test passed"
+end program
