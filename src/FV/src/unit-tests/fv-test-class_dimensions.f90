6d2f64fd60996affef3c2a47380f7ccdf662073d
Karla Morris <kmorris@sourceryinstitute.org>
2020-02-21 12:34:54 -0800

    FV: Adding test for class_dimension
    This fixes a seg fault error when using a block for calling
    the asserts with the user different operators in the dimensions class

--- src/FV/src/unit-tests/fv-test-class_dimensions.f90
+++ src/FV/src/unit-tests/fv-test-class_dimensions.f90
@@ -10,28 +10,29 @@ program test_dimensions
   !! Test dimensions class behavior
 
     use assertions_interface, only : assert
-    use class_dimensions, only : dimensions, operator(==), operator(/=), length_, time_, velocity_
+    use class_dimensions, only : dimensions, operator(==), operator(/=), length_, time_, velocity_, density_, &
+    volume_, null_dim_, mass_, energy_
 
     implicit none
 
     type(dimensions) :: height_dimensions, displacement_dimensions
-    type(dimensions) :: time_step_dimensions
+    type(dimensions) :: time_step_dimensions, time_dimensions
     type(dimensions) :: velocity_dimensions
 
     height_dimensions = length_
     displacement_dimensions = length_
     time_step_dimensions = time_
+    time_dimensions = time_
     velocity_dimensions = velocity_
 
-    test_operators: &
-    block
-      !call assert( height_dimensions == displacement_dimensions, "test_dimensions: operator(==)" )
-      !call assert( .not. (height_dimensions == time_step_dimensions), "test_dimensions: operator(==)" )
-      !call assert( height_dimensions /= time_step_dimensions, "test_dimensions: inequality operator(/=)" )
-      !call assert( displacement_dimensions / time_dimensions == velocity_, "test_dimensions: operator(/)")
-      !call assert( height_dimensions == (height_dimensions + displacement_dimensions), "test_dimensions: operator(+)")
-      !call assert( height_dimensions == (height_dimensions - length_), "test_dimensions: operator(-)")
-    end block test_operators
+    call assert( height_dimensions == displacement_dimensions, "test_dimensions: operator(==)" )
+    call assert( .not. (height_dimensions == time_step_dimensions), "test_dimensions: operator(==)" )
+    call assert( height_dimensions /= time_step_dimensions, "test_dimensions: inequality operator(/=)" )
+    call assert( displacement_dimensions / time_dimensions == velocity_, "test_dimensions: operator(/)" )
+    call assert( height_dimensions == (height_dimensions + displacement_dimensions), "test_dimensions: operator(+)" )
+    call assert( height_dimensions == (height_dimensions - length_), "test_dimensions: operator(-)" )
+    call assert( mass_ / density_ == ( volume_), "test_dimensions: operator(/)" )
+    call assert( (mass_ * velocity_) * velocity_ == ( energy_), "test_dimensions: operator(*)" )
 
     print *, "Test passed."
 
