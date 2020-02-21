d66d7b103dc5fa37202064289b3762479fae7a34
Damian Rouson <damian@sourceryinstitute.org>
2020-02-20 20:10:56 -0500

    FV: remove sample test fv-unit-test1.F90

--- src/FV/src/unit-tests/fv-unit-test1.F90
+++
@@ -1,8 +0,0 @@
-program example_fv_unit_test
-  ! use: whatever...
-  use, intrinsic :: ISO_FORTRAN_ENV, only: stdout => output_unit, stderr => error_unit
-  implicit none
-
-  write(stdout,*) "Test passed."
-  stop 0
-end program
