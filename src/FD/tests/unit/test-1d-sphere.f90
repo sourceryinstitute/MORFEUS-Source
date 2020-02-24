53877e969acb7c8778f90bf67184c69a0fbd2a71
Damian Rouson <damian@sourceryinstitute.org>
2020-02-24 15:42:33 -0800

    FD: refactor 1D-spherical heat eq solver
    1. Move almost all executable code from the main program into a
       type-bound procedure (TBP) invoked from the main program.
    2. Move the one derived-type definition to a module and make
       all components private.
    3. Move the one public procedure definition to a submodule.
    
    To Do:
    a. Add arguments to the TBP to prep for calling from FD.
    b. Add initialization and output procedures.

--- src/FD/tests/unit/test-1d-sphere.f90
+++ src/FD/tests/unit/test-1d-sphere.f90
@@ -4,142 +4,166 @@
 !     under contract "Multi-Dimensional Physics Implementation into Fuel Analysis under
 !     Steady-state and Transients (FAST)", contract # NRC-HQ-60-17-C-0007
 !
-program main
+module spherical_1D_solver_interface
+  !! author: Xiaofeng Xu, Damian Rouson, and Karla Morris
+  !! date: 2/24/2020
+  !!
+  !! Solve the 1D heat equation in spherically symmetric radial coordinates
   implicit none
+
+  private
+  public :: grid_block
+
   integer ,parameter :: digits=8  ! num. digits of kind
   integer ,parameter :: decades=9 ! num. representable decades
   integer ,parameter :: rkind = selected_real_kind(digits)
   integer ,parameter :: ikind = selected_int_kind(decades)
 
   type grid_block
+    private
     real(rkind), allocatable :: v(:,:)
     real(rkind), allocatable :: ddr2(:)
     real(rkind), allocatable :: rho(:), cp(:)
     real(rkind), allocatable :: T_analytical(:)
+  contains
+    procedure :: run_test
   end type grid_block
 
-  integer(ikind)  :: i
-  integer(ikind) ,parameter :: nr=101
+  interface
 
-  real(rkind), parameter :: h=230, Tb=293.15
+    module subroutine run_test( this )
+      implicit none
+      class(grid_block), intent(inout) :: this
+    end subroutine
 
-  type(grid_block) :: global_grid_block
-  allocate(global_grid_block%v(nr,2), global_grid_block%ddr2(nr))
-  allocate(global_grid_block%rho(nr), global_grid_block%cp(nr), global_grid_block%T_analytical(nr))
-  global_grid_block%v(:,:)=0.0
-  global_grid_block%ddr2(:)=0.0
+  end interface
 
-  associate( p => position_vectors(nr) )
-    global_grid_block%v(:,1) = p(:,1)
-  end associate
-  global_grid_block%v(:,2) = 1073.15
+end module spherical_1D_solver_interface
 
-  time_advancing: block
-    real(rkind)  :: dr_m, dz_m
-    real(rkind)  :: dr_f, dz_f
-    real(rkind)  :: dr_b, dz_b
-    real(rkind), dimension(:), allocatable :: a,b,c,d
-    real(rkind)                            :: dt,t,e, rf, rb
-
-    dt=0.1
-    t=0.0
-    do while(t<1000)
-      associate(n=>shape(global_grid_block%v))
-        allocate(a(n(1)),b(n(1)),c(n(1)),d(n(1)))
-        call get_rho(global_grid_block)
-        call get_cp(global_grid_block)
-        do i=1,n(1)
-          e=1.0/(global_grid_block%rho(i)*global_grid_block%cp(i))
-          if (i==1) then
-            dr_f=global_grid_block%v(i+1,1) - global_grid_block%v(i,1)
-            rf=0.5*(global_grid_block%v(i+1,1) + global_grid_block%v(i,1))
-            b(i)=6.0*e*kc(rf)/(dr_f**2)+1.0/dt
-            c(i)=-6.0*e*kc(rf)/(dr_f**2)
-            d(i)=global_grid_block%v(i,2)/dt
-          else if (i==n(1)) then
-            dr_b=global_grid_block%v(i,1) - global_grid_block%v(i-1,1)
-            dr_m = dr_b
-            rb=0.5*(global_grid_block%v(i,1) + global_grid_block%v(i-1,1))
-            rf=global_grid_block%v(i,1)+0.5*dr_b
-            a(i)=-1.0*e*kc(rb)*rb**2/(dr_b*dr_m*global_grid_block%v(i,1)**2)
-            b(i)=e*h*rf**2/(dr_m*global_grid_block%v(i,1)**2)+ &
-                  e*kc(rb)*rb**2/(dr_b*dr_m*global_grid_block%v(i,1)**2)+1.0/dt
-            d(i)=global_grid_block%v(i,2)/dt+e*h*Tb*rf**2/(dr_m*global_grid_block%v(i,1)**2)
-          else
-            dr_f=global_grid_block%v(i+1,1) - global_grid_block%v(i,1)
-            rf=0.5*(global_grid_block%v(i+1,1) + global_grid_block%v(i,1))
-            dr_b=global_grid_block%v(i,1) - global_grid_block%v(i-1,1)
-            dr_m = 0.5*(global_grid_block%v(i+1,1) - global_grid_block%v(i-1,1))
-            rb=0.5*(global_grid_block%v(i,1) + global_grid_block%v(i-1,1))
-            a(i)=-1.0*e*kc(rb)*rb**2/(dr_b*dr_m*global_grid_block%v(i,1)**2)
-            b(i)=e*kc(rf)*rf**2/(dr_f*dr_m*global_grid_block%v(i,1)**2)+ &
-                  e*kc(rb)*rb**2/(dr_b*dr_m*global_grid_block%v(i,1)**2)+1.0/dt
-            c(i)=-1.0*e*kc(rf)*rf**2/(dr_f*dr_m*global_grid_block%v(i,1)**2)
-            d(i)=global_grid_block%v(i,2)/dt
-          end if
-        end do
-
-        associate(U=>tridiagonal_matrix_algorithm(a,b,c,d))
-          do i=1,n(1)
-            global_grid_block%v(i,2)=U(i)
-          end do
-        end associate
-        deallocate(a,b,c,d)
-      end associate
-      t=t+dt
-    end do
-  end block time_advancing
-
-  analytical_solution: block
-    real(rkind), dimension(20)             :: mu
-    real(rkind)                            :: t,r_0, R, pi
-    real(rkind)                            :: T0, T_inf
-    integer(ikind)                         :: i,n
-
-    T0=1073.15
-    T_inf=293.15
-    t=1000.0
-    r_0=0.2
-    pi=4.0*atan(1.0)
-    do i=1, 20
-      mu(i)=(2*i-1)*pi/2.0
-    end do
-
-    do i=1,nr
-      R=global_grid_block%v(i,1)/r_0
-      global_grid_block%T_analytical(i)=0.0
-      if (i==1) then
-        do n=1, 20
-          global_grid_block%T_analytical(i)=global_grid_block%T_analytical(i)+ &
-                2.0*(sin(mu(n))-mu(n)*cos(mu(n)))/(mu(n)-sin(mu(n))*cos(mu(n)))* &
-                exp(-1.0e-5*(mu(n)/r_0)**2*t)
-        end do
-        global_grid_block%T_analytical(i)=(T0-T_inf)*global_grid_block%T_analytical(i)+T_inf
-      else
-        do n=1, 6
-          global_grid_block%T_analytical(i)=global_grid_block%T_analytical(i)+ &
-                2.0*(sin(mu(n))-mu(n)*cos(mu(n)))/(mu(n)-sin(mu(n))*cos(mu(n)))* &
-                sin(mu(n)*R)/(mu(n)*R)*exp(-1.0e-5*(mu(n)/r_0)**2*t)
-        end do
-        global_grid_block%T_analytical(i)=(T0-T_inf)*global_grid_block%T_analytical(i)+T_inf
-      end if
-    end do
-  end block analytical_solution
-
-
-  error_calculation: block
-    real(rkind)            :: avg_err_percentage
-    real(rkind), parameter :: err_percentage=0.2
-    avg_err_percentage=0.0
-    do i=1,nr
-      avg_err_percentage=avg_err_percentage+100.0*(abs(global_grid_block%v(i,2)- &
-              global_grid_block%T_analytical(i))/global_grid_block%T_analytical(i))
-    end do
-    avg_err_percentage=avg_err_percentage/nr
-    if (avg_err_percentage <= err_percentage) print *, "Test passed."
-  end block error_calculation
+submodule(spherical_1D_solver_interface) spherical_1D_solver_implementation
+  implicit none
 
 contains
+
+  module procedure run_test
+    real(rkind), parameter :: h=230, Tb=293.15
+    integer(ikind)  :: i
+    integer(ikind) ,parameter :: nr=101
+    allocate(this%v(nr,2), this%ddr2(nr))
+    allocate(this%rho(nr), this%cp(nr), this%T_analytical(nr))
+    this%v(:,:)=0.0
+    this%ddr2(:)=0.0
+
+    associate( p => position_vectors(nr) )
+      this%v(:,1) = p(:,1)
+    end associate
+    this%v(:,2) = 1073.15
+
+    time_advancing: block
+      real(rkind)  :: dr_m, dz_m
+      real(rkind)  :: dr_f, dz_f
+      real(rkind)  :: dr_b, dz_b
+      real(rkind), dimension(:), allocatable :: a,b,c,d
+      real(rkind)                            :: dt,t,e, rf, rb
+
+      dt=0.1
+      t=0.0
+      do while(t<1000)
+        associate(n=>shape(this%v))
+          allocate(a(n(1)),b(n(1)),c(n(1)),d(n(1)))
+          call get_rho(this)
+          call get_cp(this)
+          do i=1,n(1)
+            e=1.0/(this%rho(i)*this%cp(i))
+            if (i==1) then
+              dr_f=this%v(i+1,1) - this%v(i,1)
+              rf=0.5*(this%v(i+1,1) + this%v(i,1))
+              b(i)=6.0*e*kc(rf)/(dr_f**2)+1.0/dt
+              c(i)=-6.0*e*kc(rf)/(dr_f**2)
+              d(i)=this%v(i,2)/dt
+            else if (i==n(1)) then
+              dr_b=this%v(i,1) - this%v(i-1,1)
+              dr_m = dr_b
+              rb=0.5*(this%v(i,1) + this%v(i-1,1))
+              rf=this%v(i,1)+0.5*dr_b
+              a(i)=-1.0*e*kc(rb)*rb**2/(dr_b*dr_m*this%v(i,1)**2)
+              b(i)=e*h*rf**2/(dr_m*this%v(i,1)**2)+ &
+                    e*kc(rb)*rb**2/(dr_b*dr_m*this%v(i,1)**2)+1.0/dt
+              d(i)=this%v(i,2)/dt+e*h*Tb*rf**2/(dr_m*this%v(i,1)**2)
+            else
+              dr_f=this%v(i+1,1) - this%v(i,1)
+              rf=0.5*(this%v(i+1,1) + this%v(i,1))
+              dr_b=this%v(i,1) - this%v(i-1,1)
+              dr_m = 0.5*(this%v(i+1,1) - this%v(i-1,1))
+              rb=0.5*(this%v(i,1) + this%v(i-1,1))
+              a(i)=-1.0*e*kc(rb)*rb**2/(dr_b*dr_m*this%v(i,1)**2)
+              b(i)=e*kc(rf)*rf**2/(dr_f*dr_m*this%v(i,1)**2)+ &
+                    e*kc(rb)*rb**2/(dr_b*dr_m*this%v(i,1)**2)+1.0/dt
+              c(i)=-1.0*e*kc(rf)*rf**2/(dr_f*dr_m*this%v(i,1)**2)
+              d(i)=this%v(i,2)/dt
+            end if
+          end do
+
+          associate(U=>tridiagonal_matrix_algorithm(a,b,c,d))
+            do i=1,n(1)
+              this%v(i,2)=U(i)
+            end do
+          end associate
+          deallocate(a,b,c,d)
+        end associate
+        t=t+dt
+      end do
+    end block time_advancing
+
+    analytical_solution: block
+      real(rkind), dimension(20)             :: mu
+      real(rkind)                            :: t,r_0, R, pi
+      real(rkind)                            :: T0, T_inf
+      integer(ikind)                         :: i,n
+
+      T0=1073.15
+      T_inf=293.15
+      t=1000.0
+      r_0=0.2
+      pi=4.0*atan(1.0)
+      do i=1, 20
+        mu(i)=(2*i-1)*pi/2.0
+      end do
+
+      do i=1,nr
+        R=this%v(i,1)/r_0
+        this%T_analytical(i)=0.0
+        if (i==1) then
+          do n=1, 20
+            this%T_analytical(i)=this%T_analytical(i)+ &
+                  2.0*(sin(mu(n))-mu(n)*cos(mu(n)))/(mu(n)-sin(mu(n))*cos(mu(n)))* &
+                  exp(-1.0e-5*(mu(n)/r_0)**2*t)
+          end do
+          this%T_analytical(i)=(T0-T_inf)*this%T_analytical(i)+T_inf
+        else
+          do n=1, 6
+            this%T_analytical(i)=this%T_analytical(i)+ &
+                  2.0*(sin(mu(n))-mu(n)*cos(mu(n)))/(mu(n)-sin(mu(n))*cos(mu(n)))* &
+                  sin(mu(n)*R)/(mu(n)*R)*exp(-1.0e-5*(mu(n)/r_0)**2*t)
+          end do
+          this%T_analytical(i)=(T0-T_inf)*this%T_analytical(i)+T_inf
+        end if
+      end do
+    end block analytical_solution
+
+    error_calculation: block
+      real(rkind)            :: avg_err_percentage
+      real(rkind), parameter :: err_percentage=0.2
+      avg_err_percentage=0.0
+      do i=1,nr
+        avg_err_percentage=avg_err_percentage+100.0*(abs(this%v(i,2)- &
+                this%T_analytical(i))/this%T_analytical(i))
+      end do
+      avg_err_percentage=avg_err_percentage/nr
+      if (avg_err_percentage <= err_percentage) print *, "Test passed."
+    end block error_calculation
+
+  end procedure run_test
 
   pure function position_vectors(nr) result(vector_field)
     integer(ikind), intent(in) :: nr
@@ -218,5 +242,15 @@ contains
       end do
     end associate
   end subroutine
+
+end submodule spherical_1D_solver_implementation
+
+program main
+  use  spherical_1D_solver_interface, only : grid_block
+  implicit none
+
+  type(grid_block) global_grid_block
+
+  call global_grid_block%run_test
 
 end program
