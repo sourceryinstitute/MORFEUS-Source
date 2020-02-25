8ec5dc7497cffb2e4c206ee3d670231ee2fc5be0
Izaak Beekman <zbeekman@gmail.com>
2020-02-24 20:31:40 -0500

    CI: Upload debug logs, one per OS, and see where CMake is

--- cmake/SuperBuild.cmake
+++ cmake/SuperBuild.cmake
@@ -258,10 +258,11 @@ if(NOT MPI_Fortran_FOUND)
 	URL "https://www.mpich.org/static/downloads/3.3/mpich-3.3.tar.gz"
 	URL_HASH SHA256=329ee02fe6c3d101b6b30a7b6fb97ddf6e82b28844306771fa9dd8845108fa0b
 	TLS_VERIFY ON
+	SOURCE_DIR "${TPLs}/src/MPICH-src"
 	UPDATE_COMMAND ""
-	CONFIGURE_COMMAND "./configure" --enable-cache --enable-base-cache --disable-dependency-tracking --disable-cxx --enable-fortran=all
+	CONFIGURE_COMMAND "${TPLs}/src/MPICH-src/configure" --enable-cache --enable-base-cache --disable-dependency-tracking --disable-cxx --enable-fortran=all
         --disable-dependency-tracking --prefix=${TPL_DIR}
-	BUILD_IN_SOURCE ON
+	BUILD_IN_SOURCE OFF
 	TEST_BEFORE_INSTALL ON
 	TEST_COMMAND make check
 	)
